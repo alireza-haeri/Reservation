4d1d4e1b96c8964c0b63b6307461e243e3755cf1
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-06-01 12:56:06 +0330

    create domain models

---
+++ src/Example/Reservation.Cinema.Api/Models/Domain/SeatReservation.cs
@@ -0,0 +1,12 @@
+﻿namespace Reservation.Cinema.Api.Models.Domain;
+
+public class SeatReservation
+{
+    public int Id { get; set; }
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+    
+    //seat
+    public int SeatId { get; set; }
+    public Seat Seat { get; set; } = null!;
+}
