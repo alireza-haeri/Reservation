d131e87a0e92c414fbcb8d35f5bb836d9468db51
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-06-01 08:44:36 +0330

    request to api by client

--- src/Reservation.Client/Program.cs
+++ src/Reservation.Client/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿const string endpoint = "https://localhost:7073";
+const int requestCount = 1000;
 
-Console.WriteLine("Hello, World!");
+var client = new HttpClient();
+client.BaseAddress = new Uri(endpoint);
+
+var responses =Enumerable.Range(0, requestCount)
+    .Select(async (e)=> await SendRequestAsync(client, e.ToString()))
+    .ToList();
+
+await Task.WhenAll(responses);
+
+foreach (var response in responses)
+    Console.WriteLine($"Request: {response.Id}: Result: {response.Result.StatusCode}");
+
+Console.WriteLine("Completed ...");
+Console.ReadKey();
+
+return;
+
+static async Task<HttpResponseMessage> SendRequestAsync(HttpClient client, string requestId)
+{
+    Console.WriteLine($"Request: {requestId}: Sending request");
+    return await client.GetAsync("");
+}
