65c73f129b69d14760aa62b5e177c92139492c1d
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-06-01 13:27:44 +0330

    use scalar

--- src/Example/Reservation.Cinema.Api/Program.cs
+++ src/Example/Reservation.Cinema.Api/Program.cs
@@ -1,12 +1,18 @@
 using Reservation.Cinema.Api.Endpoints;
 using Reservation.Cinema.Api.Shared.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder
     .AddDbContext();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.MapCinemaHallEndpoints();
 
