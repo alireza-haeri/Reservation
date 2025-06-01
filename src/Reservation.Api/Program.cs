19d4f304c326b19fe11bff4e9772afea57a0131e
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-06-01 09:30:17 +0330

    lock with red lock redis

--- src/Reservation.Api/Program.cs
+++ src/Reservation.Api/Program.cs
@@ -1,24 +1,35 @@
 using Microsoft.EntityFrameworkCore;
+using RedLockNet.SERedis;
 using Reservation.Api.Shared.Extensions;
 using Reservation.Api.Shared.Persistence;
 
-object locker = new ();
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddDbContext();
+builder
+    .AddDbContext()
+    .AddRedLock();
 
 var app = builder.Build();
 
-app.MapGet("/", async (ReservationDbContext context) =>
+app.MapGet("/", async (ReservationDbContext context, RedLockFactory redLockFactory) =>
 {
-    lock (locker)
+    const string lockResourceName = "counter:1:lock";
+    var expireTime = TimeSpan.FromSeconds(20);
+    var waitTime = TimeSpan.FromSeconds(10);
+    var retryTime = TimeSpan.FromMicroseconds(10);
+
+    await using var redLock = await redLockFactory.CreateLockAsync(lockResourceName, expireTime, waitTime, retryTime);
+    
+    if (!redLock.IsAcquired)
     {
-        var counter = context.Counters.FirstOrDefault();
-        counter!.Increment();
-        context.SaveChanges();
-        return Results.Json(counter);
+        return Results.BadRequest();
     }
+    
+    var counter = await context.Counters.FirstOrDefaultAsync();
+    counter!.Increment();
+    await context.SaveChangesAsync();
+        
+    return Results.Json(counter);
 });
 
 app.Run();
--- src/Reservation.Api/Shared/Extensions/ServiceCollectionExtensions.cs
+++ src/Reservation.Api/Shared/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
 using Reservation.Api.Shared.Persistence;
+using StackExchange.Redis;
 
 namespace Reservation.Api.Shared.Extensions;
 
@@ -7,9 +10,21 @@ public static class ServiceCollectionExtensions
 {
     public static WebApplicationBuilder AddDbContext(this WebApplicationBuilder builder)
     {
-        builder.Services.AddDbContext<ReservationDbContext>(options=>
+        builder.Services.AddDbContext<ReservationDbContext>(options =>
             options.UseSqlServer(builder.Configuration["ConnectionStrings:Sql"]));
-        
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddRedLock(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IConnectionMultiplexer>(
+            ConnectionMultiplexer.Connect(builder.Configuration["ConnectionStrings:Redis"]));
+
+        builder.Services.AddSingleton<RedLockFactory>(sp =>
+            RedLockFactory.Create(
+                [new RedLockMultiplexer(sp.GetRequiredService<IConnectionMultiplexer>())]));
+
         return builder;
     }
 }
